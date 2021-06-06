ab4282b7ef926e71a5fd4681a05c71f0521e8168
tkperez <80167243+tkperez@users.noreply.github.com>
2021-06-06 12:50:09 -0600

    Add files via upload

---
+++ predict_churn.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from pycaret.classification import predict_model, load_model
+
+def load_data(filepath):
+    """
+    Loads diabetes data into a DataFrame from a string filepath.
+    """
+    df = pd.read_csv(filepath, index_col='Customer Number')
+    return df
+
+
+def make_predictions(df):
+    """
+    Uses the pycaret best model to make predictions on data in the df dataframe.
+    """
+    model = load_model('GBC')
+    predictions = predict_model(model, data=df)
+    predictions.rename({'Label': 'Churn_prediction'}, axis=1, inplace=True)
+    predictions['Churn_prediction'].replace({1: 'Churn', 0: 'No Churn'},
+                                            inplace=True)
+    return predictions['Churn_prediction']
+
+
+if __name__ == "__main__":
+    df = load_data('clean_churn_data_prepped.csv')
+    predictions = make_predictions(df)
+    print('predictions:')
+    print(predictions)
